785136ec4e46494c9a38b81b1ea6eed77e167c2c
Anna Spiridonova <spiridonova.aniutka@yandex.ru>
2023-05-31 14:57:56 +0300

    fix: return /

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,8 +3,8 @@ const userRouter = require('./users');
 const cardRouter = require('./cards');
 const NotFoundError = require('../errors/NotFoundError');
 
-router.use('/api', userRouter);
-router.use('/api/cards', cardRouter);
+router.use(userRouter);
+router.use('/cards', cardRouter);
 
 router.get('/signout', (req, res) => {
   res.clearCookie('jwt').send({ message: 'Выход' });
